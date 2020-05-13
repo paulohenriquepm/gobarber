a63e7e27829978ee427fdbc3ad108ee12e6ecfca
Paulo Henrique Pereira Marques <paulo.henriquepm@outlook.com>
2020-05-12 21:54:52 -0300

    first commit

---
+++ server/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '2a2cdeca7afb4c4ac120a33594be1799',
+    expiresIn: '1d',
+  },
+};
---
+++ server/src/routes/sessions-routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import CreateSessionService from '../services/CreateSessionService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  const { email, password } = request.body;
+
+  const createSession = new CreateSessionService();
+
+  const { user, token } = await createSession.execute({
+    email,
+    password,
+  });
+
+  delete user.password;
+
+  return response.json({ user, token });
+});
+
+export default sessionsRouter;
