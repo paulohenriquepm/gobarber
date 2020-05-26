c511791266ac5a39920e209b3cbacea9a58e01ea
Paulo Henrique Pereira Marques <paulinhodiv@hotmail.com>
2020-05-25 21:07:46 -0300

    Shold be able to create a new appointment

---
+++ server/src/modules/appointments/services/CreateAppointment.Service.spec.ts
@@ -0,0 +1,23 @@
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmetsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123');
+  });
+
+  // it('should not be able to create two appointments on the same datetime', () => {
+
+  // })
+});
