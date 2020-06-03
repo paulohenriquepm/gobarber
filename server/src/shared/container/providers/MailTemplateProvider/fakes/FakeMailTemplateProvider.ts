db99fa837100ee022a75607e6d84f3014909acb4
Paulo Henrique Pereira Marques <paulinhodiv@hotmail.com>
2020-06-03 20:20:28 -0300

    Email template engine using Handlebars

--- server/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ server/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IParseMailTemplateDTo from '../dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
 
 class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({ template }: IParseMailTemplateDTo): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
 
