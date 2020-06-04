c1c9292e9cb686dc1c05198cf5b3d2dc5bfd4f9c
Paulo Henrique Pereira Marques <paulinhodiv@hotmail.com>
2020-06-03 22:09:00 -0300

    UpdateProfile route and controller

--- server/src/modules/users/services/UpdateProfileService.spec.ts
+++ server/src/modules/users/services/UpdateProfileService.spec.ts
@@ -36,6 +36,16 @@ describe('UpdateProfile', () => {
     expect(updatedUser.email).toBe('johntre@example.com');
   });
 
+  it('should not be able to show the profile from non-existing user', async () => {
+    expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'Test',
+        email: 'test@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should be able to change the email to another existing email', async () => {
     fakeUsersRepository.create({
       name: 'John Doe',
