1038c0d1aad0afc07ec674f077d73dc0f14f3314
Paulo Henrique Pereira Marques <paulinhodiv@hotmail.com>
2020-06-04 10:45:20 -0300

    Filtering appointments by month

--- server/src/modules/appointments/repositories/fakes/FakeAppointmetsRepository.ts
+++ server/src/modules/appointments/repositories/fakes/FakeAppointmetsRepository.ts
@@ -1,8 +1,10 @@
 import { uuid } from 'uuidv4';
-import { isEqual } from 'date-fns';
+import { isEqual, getMonth, getYear } from 'date-fns';
 
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import ICreateAppointmentRepository from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+import ICreateAppointmentRepositoryDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
@@ -17,10 +19,25 @@ class AppointmentsRepository implements IAppointmentRepository {
     return findAppointment;
   }
 
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(
+      appointment =>
+        appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year,
+    );
+
+    return appointments;
+  }
+
   public async create({
     provider_id,
     date,
-  }: ICreateAppointmentRepository): Promise<Appointment> {
+  }: ICreateAppointmentRepositoryDTO): Promise<Appointment> {
     const appointment = new Appointment();
 
     Object.assign(appointment, { id: uuid(), date, provider_id });
