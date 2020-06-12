7e46d8c20a81e6a8692413249490c2d51d2c93c0
Paulo Henrique Pereira Marques <paulo.henriquepm@outlook.com>
2020-06-12 14:56:15 -0300

    Using redis for cache on listing providers

--- server/src/shared/container/providers/index.ts
+++ server/src/shared/container/providers/index.ts
@@ -3,11 +3,14 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
-import IMailProviderProvider from './MailProvider/models/IMailProvider';
+import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
+import ICacheProvider from './CacheProvider/models/ICacheProvider';
+import RedisCacheProvider from './CacheProvider/implementations/RedisCacheProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
@@ -19,7 +22,12 @@ container.registerSingleton<IMailTemplateProvider>(
   HandlebarsMailTemplateProvider,
 );
 
-container.registerInstance<IMailProviderProvider>(
+container.registerInstance<IMailProvider>(
   'MailProvider',
   container.resolve(EtherealMailProvider),
+);
+
+container.registerInstance<ICacheProvider>(
+  'CacheProvider',
+  container.resolve(RedisCacheProvider),
 );
